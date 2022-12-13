1784959fd8db9620065c7963a97cc822bdf3ccb9
yauster124 <59952234+yauster124@users.noreply.github.com>
2022-12-13 16:51:16 +0000

    setup files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { faker } from "@faker-js/faker";
 
-function App() {
+const words = faker.random.words(10);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Timer timeLeft={30} />
+      <GeneratedWords words={words} />;
+    </>
+  )
+}
+
+const GeneratedWords = ({ words }: { words: string}) => {
+  return (
+    <div className="text-4xl text-center text-slate-500">
+      {words}
     </div>
   );
+}
+
+const Timer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
 }
 
 export default App;
