b44f3f21d301ead6b9d1d7607f42b4f38c4b200d
yauster124 <59952234+yauster124@users.noreply.github.com>
2022-12-14 12:15:14 +0000

    added user typing component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker";
 
 import RestartButton from "./components/RestartButton";
 import Results from "./components/Results";
+import UserTypings from "./components/UserTypings";
 
 const words = faker.random.words(10);
 
@@ -9,7 +10,10 @@ const App = () => {
   return (
     <>
       <Timer timeLeft={30} />
-      <GeneratedWords words={words} />;
+      <WordsContainer>
+        <GeneratedWords words={words} />
+        <UserTypings className="absolute inset-0" userInput={words} />
+      </WordsContainer>
       <RestartButton
         className={"mx-auto mt-10 text-slate-500"}
         onRestart={() => null}
@@ -24,9 +28,17 @@ const App = () => {
   )
 }
 
+const WordsContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative max-w-xl mt-3 text-3xl leading-relaxed break-all">
+      {children}
+    </div>
+  )
+}
+
 const GeneratedWords = ({ words }: { words: string}) => {
   return (
-    <div className="text-4xl text-center text-slate-500">
+    <div className="text-slate-500">
       {words}
     </div>
   );
